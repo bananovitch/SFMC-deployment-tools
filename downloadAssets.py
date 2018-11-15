776da568996e9ee904e0f9de7a0f79de7093f297
bananovitch <piotr.michal.romanowski@gmail.com>
2018-11-15 13:56:45 +0100

    refactored authentication

--- downloadAssets.py
+++ downloadAssets.py
@@ -1,30 +1,82 @@
-import requests, config, json, io
+import requests, config, json, os
 
-payload = {
-    "clientSecret": config.accessConfig["clientSecret"],
-    "clientId":config.accessConfig["clientId"]
-}
+# prepare the target directory
+if 'assets' not in os.listdir():
+    os.makedirs('assets')
+
+
+
 #get access token
-print("Authenticating..")
-res = requests.post(config.accessConfig["authURL"], data=payload)
-res.raise_for_status()
-accessToken = json.loads(res.text)["accessToken"]
-print("Done.")
+def getToken(credentials):
+   
+    payload = {
+        "clientSecret": credentials["clientSecret"],
+        "clientId":credentials["clientId"]
+    }
+    
+    print("Authenticating..")
+    res = requests.post(credentials["authURL"], data=payload)
+    res.raise_for_status()
+    return json.loads(res.text)["accessToken"]
+    print("Done.")
+
+
+
+accessToken = getToken(config.home)
+
 headers = {
     'content-type': 'application/json',
     'Authorization': 'Bearer ' + accessToken
-    }
+}
 
-#Get assets and write them to file
+#write all the assets to a file - we might use this file as a backup source
 print("Getting assets..")
-baseURL = config.accessConfig["restBaseURL"]
+baseURL = config.home["restBaseURL"]
 
 res = requests.get(baseURL + 'asset/v1/content/assets', headers=headers)
 res.raise_for_status()
 print("Done.")
 
-assets = res.text
-targetFile = open('assets.json', 'w', encoding="utf-8")
-targetFile.write(assets)
+assetsJSON = res.text
+
+
+targetFile = open('assets/assets.json', 'w', encoding="utf-8")
+targetFile.write(assetsJSON)
 targetFile.close()
+
+print("downloading Cloud Pages..")
+assetsList = json.loads(assetsJSON)["items"]
+print(str(len(assetsList)) + " assets found.")
+
+#prepare assets to be moved
+
+landingPages = []
+templateEmails = []
+htmlEmails = []
+uncrecognized = []
+
+
+for asset in assetsList:
+    assetType = asset["assetType"]["name"]
+    if(assetType == "webpage"):
+        landingPages.append(asset)
+    elif(assetType == "templatebasedemail"):
+        templateEmails.append(asset)
+    elif(assetType == "htmlemail"):
+        htmlEmails.append(asset)
+    else:
+        uncrecognized.append(asset)
+ 
+#authenticate in target Business Unit
+accessToken = getToken(config.target)
+
+headers = {
+    'content-type': 'application/json',
+    'Authorization': 'Bearer ' + accessToken
+}
+
+print("Target BU access token: " + accessToken)
+
+
+
 
